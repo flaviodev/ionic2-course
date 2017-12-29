bfe977c9de6e46528a37542503ba5f795754b15c
flavio <fdsdev@gmail.com>
2017-12-29 06:18:12 -0200

    toogle and actions

--- aluraCar/src/pages/choose/choose.ts
+++ aluraCar/src/pages/choose/choose.ts
@@ -8,11 +8,28 @@ import { NavParams } from 'ionic-angular';
 export class ChoosePage implements OnInit {
     
     public selectedCar;
+    public accessories;
+    private _totalAmount: number;
+
+    get totalAmount(): number {
+        return this._totalAmount;
+    }
 
     constructor(public navParams:NavParams) {}
 
     ngOnInit() {
         this.selectedCar = this.navParams.get("selectedCar");
+        this._totalAmount = this.selectedCar.preco;
+        this.accessories = [
+            { name: 'ABS Brake', price: 800 },
+            { name: 'Air Conditioning', price: 1000 },
+            { name: 'MP3 Player', price: 500 }
+        ];
     }
 
+    updateTotal(on: boolean, accesssory) {
+        on ? 
+            this._totalAmount+=accesssory.price :
+            this._totalAmount-=accesssory.price;
+    }
 }
