b5e0edf4b83c532cf7a99e9f582227427e7060a7
flavio <fdsdev@gmail.com>
2017-12-29 04:42:30 -0200

    requesting rest api

--- aluraCar/src/pages/home/home.ts
+++ aluraCar/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http'
+
 
 @Component({
   selector: 'page-home',
@@ -9,22 +11,14 @@ export class HomePage {
 
   public carros;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private _http: Http) {
 
-    this.carros = [
-        {nome:'Azera V6',preco:85000},
-        {nome:'Onix 1.6',preco:35000},
-        {nome:'Fiesta 2.0',preco:52000},
-        {nome:'C3 1.0',preco:22000},
-        {nome:'Uno Fire',preco:11000},
-        {nome:'Sentra 2.0',preco:53000},
-        {nome:'Astra Sedan',preco:39000},
-        {nome:'Vectra 2.0 Turbo',preco:37000},
-        {nome:'Hilux 4x4',preco:90000},
-        {nome:'Montana Cabine dupla',preco:57000},
-        {nome: 'Outlander 2.4',preco:99000},
-        {nome:'Brasilia Amarela',preco:9500},
-        {nome:'Omega Hatch',preco:8000}
-    ];
+    this._http
+      .get('https://aluracar.herokuapp.com')
+      .map(res => res.json())
+      .toPromise()
+      .then(carros => this.carros = carros);
+
+    console.log(this.carros);
   }
 }
