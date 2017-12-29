bc71962295a305e8ba74e92ab0ff4d703638c95d
flavio <fdsdev@gmail.com>
2017-12-29 04:50:21 -0200

    using loading controller

--- aluraCar/src/pages/home/home.ts
+++ aluraCar/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http'
 
 
@@ -11,14 +11,24 @@ export class HomePage {
 
   public carros;
 
-  constructor(public navCtrl: NavController, private _http: Http) {
+  constructor(
+    public navCtrl: NavController, 
+    private _http: Http,
+    private _loadingCtrl: LoadingController) {
+
+    let loader = _loadingCtrl.create({
+      content: "Getting new cars. Waiting ..."
+    });
+
+    loader.present();
 
     this._http
       .get('https://aluracar.herokuapp.com')
       .map(res => res.json())
       .toPromise()
-      .then(carros => this.carros = carros);
-
-    console.log(this.carros);
+      .then(carros => { 
+        this.carros = carros;
+        loader.dismiss();
+      });
   }
 }
