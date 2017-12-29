b232617da09690fcfcd61ca5d5dfdd8e35b68bdb
flavio <fdsdev@gmail.com>
2017-12-29 14:50:37 -0200

    submit form

--- aluraCar/src/pages/registry/registry.ts
+++ aluraCar/src/pages/registry/registry.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+
 import { Car } from './../../domain/car/car';
 
 @Component({
@@ -10,15 +12,41 @@ export class RegistryPage implements OnInit {
   public car: Car;
   private totalAmount: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  private schedule;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private _http: Http) {}
 
   ngOnInit() {
     this.car = this.navParams.get("car");
     this.totalAmount = this.navParams.get("totalAmount");
+    this.schedule = {date: new Date().toISOString()};
   }
 
   // just firs time
   ionViewDidLoad() {
     console.log('ionViewDidLoad RegistryPage');
+  }
+
+  toSchedule() {
+    let api = "https://aluracar.herokuapp.com/salvarpedido?carro="+
+              this.car.nome
+              +"&nome="+
+              this.schedule.name
+              +"&preco="+
+              this.totalAmount
+              +"&endereco="+
+              this.schedule.address
+              +"&email="+
+              this.schedule.email
+              +"&dataAgendamento="+
+              this.schedule.date;
+    this._http
+    .get(api)
+    .toPromise()
+    .then(() => alert('sucesso'))
+    .catch(err => alert(err));
   }
 }
